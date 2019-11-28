f559b5870a0ea00c30b18eb72d0a7e06166683c9
Daniel Tamargo <danieltamrgosaiz@gmail.com>
2019-11-28 21:45:18 +0100

    Apuntes

--- Ejercicios/Diccionarios I/venv/Ejercicios.py
+++ Ejercicios/Diccionarios I/venv/Ejercicios.py
@@ -1,4 +1,4 @@
-# Quitar Tildes
+# Quitar Tildes utilizando un Diccionario
 tildes = {'á' : 'a', 'à' : 'a'}
 palabra = 'hálà'
 print("Antes de quitar:", palabra)
@@ -6,4 +6,15 @@ for i in range(0, len(palabra)):
     if palabra[i] in tildes.keys():
         palabra = palabra.replace(palabra[i], tildes[palabra[i]])
 print("Después de quitar:", palabra)
+
+import funciones
+
+# Funciones en un Diccionario
+diccionario_funciones = {'a' : funciones.es_par, 'b' : funciones.es_primo}
+
+numero = 11
+if diccionario_funciones['a'](numero):
+    print("Es par")
+else:
+    print("Es impar")
 
---
+++ Ejercicios/Diccionarios I/venv/funciones.py
@@ -0,0 +1,9 @@
+def es_par(n):
+    return n % 2 == 0
+
+def es_primo(n):
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return false
+    return true
+        
